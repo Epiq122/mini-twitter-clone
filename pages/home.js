9ce855a95d9cd2ea52d1c64dbf27935169b4d947
Robert Gleason <l2ob122@gmail.com>
2023-04-27 22:03:34 -0700

    created Tweet components, added functionalty to talk to backend to get tweets

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,33 @@
 import { useSession } from "next-auth/react";
 import NewTweet from "@/components/NewTweet";
+import { useRouter } from "next/router";
+import Tweets from "@/components/Tweets";
+import { getTweets } from "@/lib/data";
 
-export default function Home() {
+export default function Home({ tweets }) {
   const { data: session, status } = useSession();
-  return <div>{session ? <NewTweet /> : <p>Not logged in!</p>}</div>;
+  const loading = status === "loading";
+  const router = useRouter();
+  if (loading) {
+    return null;
+  }
+  if (!session) {
+    router.push("/");
+  }
+  return (
+    <>
+      <NewTweet />
+      <Tweets tweets={tweets} />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  let tweets = await getTweets(prisma);
+  tweets = JSON.parse(JSON.stringify(tweets));
+  return {
+    props: {
+      tweets,
+    },
+  };
 }
