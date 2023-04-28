4bf955a443d274697b10edb8195306c2943f5e55
Robert Gleason <l2ob122@gmail.com>
2023-04-27 19:28:13 -0700

    added /home

---
+++ pages/home.js
@@ -0,0 +1,8 @@
+import { useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session, status } = useSession();
+  return (
+    <div>{session ? <p>You are logged in!</p> : <p>Not logged in!</p>}</div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return null;
+  }
+  if (session) {
+    router.push("/home");
+  }
   return (
     <div>
       <h1>Home Page</h1>
